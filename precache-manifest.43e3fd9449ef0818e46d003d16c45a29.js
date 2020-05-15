65db70c8ecb70e57f4dbeb079dafc44941de3e6c
lsw1164 <qkqkfo@gmail.com>
2020-05-16 01:25:44 +0900

    Updates

---
+++ precache-manifest.43e3fd9449ef0818e46d003d16c45a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53b308133911ab19e47676891674ccc",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8a06878de19027fa2b4d",
+    "url": "/movie_app/static/css/main.9728e8eb.chunk.css"
+  },
+  {
+    "revision": "d84edc655e1cbd4b99c8",
+    "url": "/movie_app/static/js/2.069a2fcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.069a2fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a06878de19027fa2b4d",
+    "url": "/movie_app/static/js/main.bb21d2d8.chunk.js"
+  },
+  {
+    "revision": "5b6a3b721d1b00f72fb2",
+    "url": "/movie_app/static/js/runtime-main.5067d86a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.f87c2df64a8b6edcfd6ceddb19bbedb5.js"
+  "/movie_app/precache-manifest.43e3fd9449ef0818e46d003d16c45a29.js"
 );
 
 self.addEventListener('message', (event) => {
